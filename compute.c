706ecffd7d0fcfb6a9cb1240209417b116dcf30e
Shubhi251810 <shubhichaudhary822@gmail.com>
2024-10-04 11:58:30 +0530

    Add files via upload

---
+++ compute.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int  a,b,c,x;
+	printf("enetr the value of a");
+	scanf("%d",&a);
+		printf("enetr the value of b");
+	scanf("%d",&b);
+		printf("enetr the value of c");
+	scanf("%d",&c);
+	x=(a+b+c)/3;
+	printf("value of x is : %d",x);
+	getch();
+}
