d792710afda1dd03a1e89276ee816cd52badb327
Shubhi251810 <shubhichaudhary822@gmail.com>
2024-12-12 13:43:29 +0530

    Add files via upload

---
+++ add2num.c
@@ -0,0 +1,14 @@
+// WAP to input and add two numbers
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum=0;
+	printf("enter the first number:");
+	scanf("%d",&num1);
+	printf("enter the second number:");
+	scanf("%d",&num2);
+	sum=num1+num2;
+	printf("the sum is %d",sum);
+	return 0;
+}
+
